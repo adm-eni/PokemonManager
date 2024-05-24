d675aab0922224e7832a24793f79c8a8e1d79bef
Adam <adam.siran2022@campus-eni.fr>
2024-05-24 18:09:03 +0200

    Initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GenerationsComponent} from "./src/components/generations/generations.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GenerationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
